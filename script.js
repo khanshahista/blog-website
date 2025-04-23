bf46f2accb690a71ea7632ff92e1a1dea3ef7555
khanshahista <shaista6351@gmail.com>
2025-04-23 13:19:38 +0530

    script.js

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const blogPosts = [
   {
     "title": "DBMS",
     "snippet": "Feature of DBMS.",
-    "keyword": "",
-    "details": "Organizes and manages data,Ensures security and integrity,Supports multi-user access,Provides backup and recovery,Uses SQL for queries,Reduces data redundancy."
+    "keyword": "Data base management system",
+    "details": "Organizes and manages data."
   },
   {
     "title": "Exploring Android OS",
