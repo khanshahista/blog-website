70f58567a86b9424bd254050502ce563fe49d572
khanshahista <shaista6351@gmail.com>
2025-04-23 12:49:14 +0530

    script.js

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const blogPosts = [
     "keyword": "python Book",
     "details": "Python is a popular, high-level programming language known for its simplicity and readability. It's widely used in areas like web development, data science, automation, and AI. Python has an easy-to-learn syntax, strong community support, and a large set of libraries that make it great for both beginners and professionals."
   },
-Let me know if you want help learning it or building something with it!."
+
+  "Let me know if you want help learning it or building something with it!."
   {
     "title": "Exploring Android OS",
     "snippet": "Android OS powers billions of devices worldwide.",
